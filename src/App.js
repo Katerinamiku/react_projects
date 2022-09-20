04f6f7eb0d088d7d259c39b3f0c4531b5b4af281
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-09-20 15:32:31 +0200

    modal window

--- src/App.js
+++ src/App.js
@@ -1,75 +1,44 @@
-import './index.scss';
 import React, {useState} from 'react';
+import './index.scss';
+import Modal from "./Modal";
 
-const questions = [
-    {
-        title: 'React - is ... ?',
-        variants: ['a library', 'a framework', 'an application'],
-        correct: 0,
-    },
-    {
-        title: 'Component - is ... ',
-        variants: ['an application', 'a part of an application or a page', '...I don\'t know what it is'],
-        correct: 1,
-    },
-    {
-        title: 'What is JSX?',
-        variants: [
-            'simple HTML',
-            'function',
-            'HTML, but able to manage JS-code',
-        ],
-        correct: 2,
-    },
-];
 
-function Result({correct, onClickStart}) {
-    return (
-        <div className="result">
-            <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
-            <h2>You named {correct} correct answers from {questions.length}</h2>
-            <button onClick={onClickStart}>Try again</button>
-        </div>
-    );
-}
-
-function Game({step, question, onClickVariant}) {
-
-    const percentage = Math.round((step / questions.length) * 100)
-    return (
-        <>
-            <div className="progress">
-                <div style={{ width: `${percentage}%` }} className="progress__inner"></div>
-            </div>
-            <h1>{question.title}</h1>
-            <ul>
-                {question.variants.map((text, index) => <li key={text} onClick={() => onClickVariant(index)}>{text}</li>)}
-            </ul>
-        </>
-    );
-}
-
+//------------------------classes------------------------------
 function App() {
-    const [step, setStep] = useState(0);
-    const question = questions[step];
-    const onClickVariant = (index) => {
-        setStep(step+1)
-        if (index === question.correct) {
-            setCorrect(correct+1)
-        }
-    }
-    const [correct, setCorrect] = useState(0);
-    const onClickStart = () => {
-        setStep(0);
-        setCorrect(0)
-    }
+    const [open, setOpen] = useState(false);
+
     return (
-        <div className="App">
-            {step !== questions.length
-                ? <Game step={step} question={question} onClickVariant={onClickVariant}/>
-                : <Result correct={correct} onClickStart={onClickStart}/>}
-        </div>
+        <Modal open={open} setOpen={setOpen}>
+            <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+            <h3>This is modal</h3>
+        </Modal>
     );
 }
 
 export default App;
+
+//-------------------------Conditional Rendering------------------------
+// function App() {
+//
+//     const [open, setOpen] = useState(false);
+//     const onClickHandler = () => {
+//         setOpen(!open)
+//     }
+//
+//   return (
+//     <div className="App">
+//       <button onClick={onClickHandler} className="open-modal-btn">✨ Открыть окно</button>
+//       {open && <div className="overlay">
+//         <div className="modal">
+//           <svg onClick={onClickHandler} height="200" viewBox="0 0 200 200" width="200">
+//             <title />
+//             <path d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z" />
+//           </svg>
+//           <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
+//         </div>
+//       </div>}
+//     </div>
+//   );
+// }
+//
+// export default App;
